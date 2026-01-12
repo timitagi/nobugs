67d07bba3e676a63f7e1ea21c1645e4ba363b98b
timitagi <t.t.iskyandyarov@gmail.com>
2026-01-12 17:52:52 +0300

    Создал LargestofNumbers

---
+++ src/main/java/homework_conditional_statements/largest_of_numbers/LargestOfNumbers.java
@@ -0,0 +1,23 @@
+package homework_conditional_statements.largest_of_numbers;
+
+import java.util.Scanner;
+
+public class LargestOfNumbers {
+    public static void main(String[] args){
+        largestOfNumbers();
+
+    }
+
+    public static void largestOfNumbers(){
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        String description = "";
+        if(number1>number2){
+            description = "Первое число больше второго";
+        } else {
+            description = "Второе число больше первого";
+        }
+        System.out.println(description);
+    }
+}
