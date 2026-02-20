14e0960c09881f7dbde188df66430735e7dab5ca
timitagi <t.t.iskyandyarov@gmail.com>
2026-02-20 20:32:21 +0300

    Задача №1, hashset

---
+++ src/main/java/homework_collections/hashset/task1/Main.java
@@ -0,0 +1,16 @@
+package homework_collections.hashset.task1;
+
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args){
+        HashSet<Integer> set = new HashSet<>();
+        set.add(3);
+        set.add(2);
+        set.add(1);
+        set.add(4);
+        set.add(5);
+        set.add(100);
+        System.out.println(set);
+    }
+}
