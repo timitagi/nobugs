ee2aef8b4d090c448d2efeb79a11e54a6fcaef6f
timitagi <t.t.iskyandyarov@gmail.com>
2026-02-20 19:58:21 +0300

    Задача №1, linkedlist

---
+++ src/main/java/homework_collections/linkedlist/task1/Main.java
@@ -0,0 +1,12 @@
+package homework_collections.linkedlist.task1;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args){
+        LinkedList<String> list = new LinkedList<>(Arrays.asList("Привет","Я","Иду","Гулять","На улицу"));
+        System.out.println(list);
+
+    }
+}
