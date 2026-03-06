750467a7e2c46a0ab773872bb906e3add8a6315c
timitagi <t.t.iskyandyarov@gmail.com>
2026-03-06 19:18:02 +0300

    Задачи 2

---
+++ src/main/java/homework_exceptions_generics/task2/Main.java
@@ -0,0 +1,24 @@
+package homework_exceptions_generics.task2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число а");
+        int a = scanner.nextInt();
+        System.out.println("Введите число b");
+        int b = scanner.nextInt();
+        divide(a,b);
+
+    }
+
+    public static void divide(int a, int b){
+        try {
+            int result = a/b;
+            System.out.println(result);
+        } catch (ArithmeticException e){
+            System.out.println("На ноль делить нельзя!");
+        }
+    }
+}
